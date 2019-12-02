a4899d3351e7bdd3b22dd3bc708514cb6b8ba657
Podlovics Péter Dávid <anabra@INF.ELTE.HU>
2019-12-02 12:39:04 +0100

    Added Practice12 1214 PRE

---
+++ 2019-2020-1/1214/Practice12.hs
@@ -0,0 +1,31 @@
+module Practice12 where
+
+sum1 :: [Int] -> Int 
+sum1 []     = 0 
+sum1 (x:xs) = x + sum1 xs
+
+-- sum1 [1,2,3] ~ 
+-- foldr_ (+) 0 [1,2,3] == 1 + (2 + (3 + (0)))
+foldr_ :: (a -> b -> b) -> b -> [a] -> b
+foldr_ f neut []     = neut 
+foldr_ f neut (x:xs) = f x (foldr_ f neut xs)
+
+-- tail recursion
+sum2' :: Int -> [Int] -> Int
+sum2' acc []     = acc
+sum2' acc (x:xs) = sum2' (acc + x) xs
+
+sum2 :: [Int] -> Int
+sum2 = sum2' 0
+
+-- sum2 [1,2,3] 
+-- foldl_ (+) 0 [1,2,3] = (((0) + 1) + 2) + 3
+foldl_ :: (b -> a -> b) -> b -> [a] -> b
+foldl_ f acc []     = acc
+foldl_ f acc (x:xs) = foldl_ f (f acc x) xs
+
+-- concat, maximum, length, isort, (++), reverse
+-- for isort, use Data.List.insert
+
+-- isort [3,2,7] == insert 3 (insert 2 (insert 7 []))
+-- isort [3,2,7] == insert 3 $ insert 2 $ insert 7 $ [] 
